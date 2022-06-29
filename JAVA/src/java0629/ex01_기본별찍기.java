d6b71670fcd15adcf518acdb07302c6e43d91066
안요한 <93561179+yohan-is-free@users.noreply.github.com>
2022-06-29 12:37:03 +0900

    이중for문
    이중for문 실습

---
+++ JAVA/src/java0629/ex01_기본별찍기.java
@@ -0,0 +1,15 @@
+package java0629;
+
+public class ex01_기본별찍기 {
+
+	public static void main(String[] args) {
+
+		for(int j=1;j<=5;j++) {
+			for (int i = 1; i <= j; i++) {
+				System.out.print("*");
+			}	
+			System.out.println();
+		}
+		
+	}
+}
